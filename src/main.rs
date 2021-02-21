c44c33d63af8c20e738002a52c7078d2300e8830
F0X <f0xde@protonmail.com>
2021-02-21 17:40:46 +0100

    Copy default config

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,30 @@ mod db;
 mod server;
 
 use anyhow::Result;
+use log::info;
 use serde::Deserialize;
-use tokio::fs;
+use std::path::Path;
+use tokio::{
+    fs::{self, File},
+    io::AsyncWriteExt,
+};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     pretty_env_logger::init();
 
-    let config: Config = toml::from_str(&fs::read_to_string("servery.toml").await?)?;
+    let config_path = Path::new("servery.toml");
+    if !config_path.exists() {
+        info!("Writing default config to {:?}", config_path);
+        if let Some(parent) = config_path.parent() {
+            fs::create_dir_all(parent).await?;
+        }
+        File::create(config_path)
+            .await?
+            .write_all(include_bytes!("../default_config.toml"))
+            .await?;
+    }
+    let config: Config = toml::from_str(&fs::read_to_string(config_path).await?)?;
 
     let db = mongodb::Client::with_uri_str(&config.db_uri)
         .await?
